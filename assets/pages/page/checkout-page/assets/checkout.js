2b5032431fc3dac09ae7c411fef49352807900b5
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-05-19 11:21:40 +0300

    added an order summary that will contain the items the user has picked from the products page.the user will also input details about him/her and address.

---
+++ assets/pages/page/checkout-page/assets/checkout.js
@@ -0,0 +1,9 @@
+function addBread() {
+    var product = document.getElementById("product")
+    var quantity = document.getElementById("quantity")
+    var price = document.getElementById("price")
+
+    product.innerText = ("Bread");
+    quantity.innerText = (1);
+    price.innerText = (45 + " ksh")
+}
